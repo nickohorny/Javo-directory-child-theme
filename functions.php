c35fbafd82619ceace0f53f058487205a1258d5e
nickohorny <nicko19822@gmail.com>
2015-02-04 14:41:17 +0700

    initial command

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+// Translate function for child theme 
+// if you need to translate strings in child theme, please remove these comment and use this function
+
+// load_child_theme_textdomain( 'javo_fr', get_stylesheet_directory() . '/languages' );
